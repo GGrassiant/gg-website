8da7e84e0887107ac73cdf9f5d911b602cc9cdf4
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2020-07-23 15:53:07 -0400

    Add dynamic project template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+      query {
+      allContentfulProject {
+        edges {
+          node {
+            id
+            node_locale
+            shortDescription
+            slug
+          }
+        }
+      }
+    }`
+  );
+
+  if (result.errors) {
+    return reporter.panicOnBuild(`Error while running GraphQL query.`);
+  }
+
+  const ProjectTemplate = path.resolve(`src/templates/Project/Project.tsx`);
+
+  result.data.allContentfulProject.edges.forEach(({ node }) => {
+    const lang = `${node.node_locale}/`;
+    const pagePath = `${lang}project/${node.slug}`;
+
+    createPage({
+      path: pagePath,
+      component: ProjectTemplate,
+      context: {
+        pagePath,
+        slug: node.slug,
+        lang: node.node_locale,
+        id: node.id,
+      },
+    })
+  })
+};
---
+++ src/templates/Project/Project.tsx
@@ -0,0 +1,23 @@
+// Libs
+import React from 'react';
+import { graphql } from "gatsby"
+
+const Project: React.FC<any> = ({data: { contentfulProject: { title } }}) => {
+  return(
+    <div>
+      Coucou, {title}
+    </div>
+  );
+}
+
+export default Project;
+
+export const query = graphql`
+  query ProjectQuery($id: String!) {
+    contentfulProject(
+    id: { eq: $id }
+    ) {
+      title
+    }
+  }
+`;
