d04a7fec940e5144c12091f412525617f67e933f
ggrassiant <guillaume.grassiant@gmail.com>
2020-10-25 17:55:57 -0400

    remove obselete test

--- src/pages/__tests__/index.js
+++ src/pages/__tests__/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // utils
-import { render, screen } from '../../components/__tests__/utils/test-utils';
+import { render } from '../../components/__tests__/utils/test-utils';
 
 // Component
 import IndexPage from '../index';
@@ -12,8 +12,6 @@ describe('<Index>', () => {
       const { container } = render(<IndexPage />);
       expect(container).toBeInTheDocument();
       expect(container.querySelector('h1')).toBeInTheDocument();
-      expect(container.querySelector('img')).toBeInTheDocument();
-      expect(screen.getByText('Go to page 2')).toBeInTheDocument();
     });
   });
 });
