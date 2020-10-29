1b9ad9fe1a0999608db435e1a71face72671a069
ggrassiant <guillaume.grassiant@gmail.com>
2020-10-29 10:07:27 -0400

    test

--- src/components/Link/link-styles.tsx
+++ src/components/Link/link-styles.tsx
@@ -15,7 +15,8 @@ const customColor: { [key: string]: Property.Color } = {
 };
 
 export const LinkWrapper = styled.a<LinkProps>`
-  font-family: ${variables.defaultContentFont};
+  font-family: Arial;
+  //font-family: ${variables.defaultContentFont};
   font-weight: ${variables.fontWeightRegular};
   font-size: ${variables.fontSizeDefault};
   line-height: 13px;
