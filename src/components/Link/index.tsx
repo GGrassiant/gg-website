414e0140330055248594b621b5e44c33e40d6dbe
ggrassiant <guillaume.grassiant@gmail.com>
2020-10-29 13:50:32 -0400

    try css variables

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -6,11 +6,13 @@ import { LinkProps } from './index-types';
 
 // Styles
 import { LinkWrapper } from './link-styles';
+import styles from '../../pages/index.module.scss';
 
 const Link: React.FC<LinkProps> = (props) => {
   const { children, theme = 'dark', href } = props;
   return (
     <LinkWrapper theme={theme} target="_blank" href={href}>
+      <p className={styles.lolz}>Coucou</p>
       {children}
     </LinkWrapper>
   );
