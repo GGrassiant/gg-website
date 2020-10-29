b227f4ec9b80bebe00d7693396cfeaf73e998425
ggrassiant <guillaume.grassiant@gmail.com>
2020-10-29 09:37:05 -0400

    test

--- src/components/Link/link-styles.tsx
+++ src/components/Link/link-styles.tsx
@@ -18,6 +18,7 @@ export const LinkWrapper = styled.a<LinkProps>`
   //font-family: ${variables.defaultContentFont};
   font-weight: ${variables.fontWeightRegular};
   //font-size: ${variables.fontSizeDefault};
+  font-size: 30px;
   line-height: 13px;
   color: ${(props) => customColor[props.theme]};
   text-transform: capitalize;
