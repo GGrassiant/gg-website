b6b48a5d28cbf17ee3dda4630dd9d0ecb0191c88
ggrassiant <guillaume.grassiant@gmail.com>
2020-10-29 13:35:14 -0400

    add root css

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -6,13 +6,12 @@ import { LinkProps } from './index-types';
 
 // Styles
 import { LinkWrapper } from './link-styles';
-import styles from '../../pages/index.module.scss';
 
 const Link: React.FC<LinkProps> = (props) => {
   const { children, theme = 'dark', href } = props;
   return (
     <LinkWrapper theme={theme} target="_blank" href={href}>
-      <p className={styles.testlolx}>{children}</p>
+      <p>{children}</p>
     </LinkWrapper>
   );
 };
